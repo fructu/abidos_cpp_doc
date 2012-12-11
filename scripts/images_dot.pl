40e14fc1484e1a2548e34684771b4330a4fd1cf5
hevia <fructu@gmail.com>
2012-12-11 19:19:46 +0100

    improve Makefile to generate images from dot files

---
+++ scripts/images_dot.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl -w
+#
+#
+#
+#
+
+
+use strict;
+
+sub abidos
+{
+  my $file = shift;
+  my $file_output = shift;
+  my $command = "";
+
+  print "  file_output[$file_output.svg]\n";
+
+  $command= "  cat $file | dot -Tsvg > out/images/$file_output.svg";
+  system($command);
+
+  print "  file_output[$file_output.eps]\n";
+  $command= "  cat $file | dot -Teps > out/images/$file_output.eps";
+  system($command);  
+}
+
+sub main
+{
+  my $file = shift;
+  my $output_name = "";
+
+  print "images_dot.pl\n";
+  print "{\n";
+  print "  file[$file]\n";
+
+
+  if( $file =~ /\.\/([\S]*).(gv|dot)/)
+  {
+    $output_name = $1;
+    $output_name =~ s/\//_/g;
+
+    abidos($file, $output_name);
+  }
+
+  print "}\n";
+}
+
+main($ARGV[0]);
